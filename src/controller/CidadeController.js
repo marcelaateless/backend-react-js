18ad1bca9141c011913431bbe118bd41f099bcf8
Marcela Teles <marcela@gmail.com>
2026-05-22 19:16:15 -0300

    Criação métodos cadastrar e listas cidades +Rotas para realização desses métodos

---
+++ src/controller/CidadeController.js
@@ -0,0 +1,28 @@
+const CidadeModel = require('../model/CidadeModel');
+
+class CidadeController {
+    async create(req, res) {
+        try{
+            const cidade = await CidadeModel.create(req.body);
+            return res.status(201).json(cidade);
+        } catch(error){
+            return res.status(500).json({
+                erro: 'Erro ao criar cidade',message: error.message
+            })
+        }
+    }
+
+    async  all (req, res) {
+        try {
+            const cidades = await CidadeModel.findAll({
+                order:[['nome','ASC']]
+            });
+            return res.status(200).json(cidades);
+        }catch(err){
+            return res.status(500).json({error: 'Erro ao buscar cidade',message: err.message})
+        }
+    }
+
+}
+
+module.exports = new CidadeController;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 
-server.get('/teste', (req, res) => {
-    res.send('Tudo certo com a API!');
-});
+const CidadeRoutes = require('./routes/CidadeRoutes')
+
+server.use(express.json());
+
+server.use('/cidade',CidadeRoutes);
 
 server.listen(3000, () => {
     console.log("API ONLINE");
